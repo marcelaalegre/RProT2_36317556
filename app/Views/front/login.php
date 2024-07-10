3a33d6d8619f0c3ec588d0c4a2628ff3d330c779
marcelaalegre <marcelita.alegre23@gmail.com>
2024-07-09 23:32:36 -0300

    Styles

--- app/Views/front/login.php
+++ app/Views/front/login.php
@@ -1,20 +1,21 @@
-<div class="container mt-5 mb-5 de-flex justify-content-center">
+<div class="container mt-2 mb-2 de-flex justify-content-center" style= "max-width: 540px;">
 <div class="card" style="width: 100%">
 <div class="card-header text-center">
     <h2>Iniciar Sesion</h2>
 </div>
 <!--Inicio del Formulario de Login-->
 <form class="post">
-    <div class="card-body" media="(max-width:600px)">
-        <div class="mb-2">
+    <div class="card-body">
+        <div class="mb-2" style= "display: flex; flex-direction: column;">
             <label for="exampleFormControlInput1" class="form-label">Correo</label>
-            <input name="Email" type="text" class="Form-control" placeholder="Correo">
+            <input name="Email" type="email" class="Form-control" placeholder="Correo">
         </div>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Password</label>
             <input name="Pass" type="password" class="Form-control" placeholder="Contraseña">
         </div>
-        <input type="submit" value="Ingresas" class="btn btn-success">
+        <input type="submit" value="Ingresar" class="btn btn-success">
+        <input type="submit" value="Cancelar" class="btn btn-danger">
         <br>    
         <span>¿Aun no se registro?<a href="<?php echo base_url('/Registro');?>">Registrarse aqui</a></span>
     </div>
